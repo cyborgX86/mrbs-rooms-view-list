495bd13b7a443c2cba25208b50734068ac4bc50c
n7rc <notrec@hotmail.com>
2018-04-30 12:03:19 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-//Pendiente: revisar css para quitar propiedades sin uso.
-
 require_once ("config.inc.php");
 require_once ("functions.inc.php");
 
@@ -15,27 +13,27 @@ date_add ($dateEnd, date_interval_create_from_date_string('' . $dateRange . 'day
 $dateEnd = date_format($dateEnd, 'Y-m-d');
 
 echo '<!DOCTYPE html>
-			<html>
-			<head>
-			<meta charset="UTF-8">
-			<link rel="stylesheet" type="text/css" href="css/style.css" />
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>' . $titlePage . '</title>
-			<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
-			<script type="text/javascript" src="js/infinite.scroll.js"></script>
-			</head>
-			<body>';
+      <html>
+      <head>
+      <meta charset="UTF-8">
+      <link rel="stylesheet" type="text/css" href="css/style.css" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>' . $titlePage . '</title>
+      <script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+      <script type="text/javascript" src="js/infinite.scroll.js"></script>
+      </head>
+      <body>';
 
 echo '<div id="booking-table">
-			<div class="bookings" id="main">
-			<h3>' . $title;
+      <div class="bookings" id="main">
+      <h3>' . $title;
 
 if (occupationState(qryState($date, $dateEnd, $mrbsIdRoom1, $mrbsIdRoom2, $mrbsIdRoom3)) == 1){
-	echo '<span class="status"><font color="#4267b2">PRÓXIMAMENTE</font></span></h3>';
+  echo '<span class="status"><font color="#4267b2">PRÓXIMAMENTE</font></span></h3>';
 
 }else{
-	echo '<span class="status"><font color="#4267b2">SIN ACTIVIDADES EN PRÓXIMOS DÍAS</font>
-				</span></h3>';
+  echo '<span class="status"><font color="#4267b2">SIN ACTIVIDADES EN PRÓXIMOS DÍAS</font>
+        </span></h3>';
 }
 
 printOccupationTable(qry($date, $dateEnd, $mrbsIdRoom1, $mrbsIdRoom2, $mrbsIdRoom3));
